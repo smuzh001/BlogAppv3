62377f03e643895a972e9ce79d198184f36a6bea
smuzh001 <smuzh001@ucr.edu>
2019-08-05 14:52:03 -0700

    Initial commit

---
+++ app/src/main/java/com/smuzh001/blogappv3/BlogFeed.java
@@ -0,0 +1,13 @@
+package com.smuzh001.blogappv3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlogFeed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_feed);
+    }
+}
---
+++ app/src/main/java/com/smuzh001/blogappv3/MainActivity.java
@@ -0,0 +1,76 @@
+package com.smuzh001.blogappv3;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.smuzh001.blogappv3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String email;
+    private String password;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView emailField = findViewById(R.id.emailField);
+        final TextView passwordField = findViewById(R.id.passwordField);
+        Button loginButton = findViewById(R.id.LoginButton);
+        Button newAccButton = findViewById(R.id.newAccButton);
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailField.getText().toString();
+                password = passwordField.getText().toString();
+                login(email, password);
+            }
+        });
+        newAccButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    public void login(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Login", "signInWithEmail:success");
+                            Intent intent = new Intent(getApplicationContext(), BlogFeed.class);
+                            startActivity(intent);
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Login", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+}
